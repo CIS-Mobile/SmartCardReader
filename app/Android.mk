4ac85fcd245f5777798f44bebdd717c2196fb79b
Rashed Abdel-Tawab <rabdeltawab@cismobile.com>
2021-02-11 11:29:47 -0500

    Convert ConstraintLayout to androidx and build with Android.mk

---
+++ app/Android.mk
@@ -0,0 +1,33 @@
+ifeq ($(BUILD_AS_GUARD_PHONE), true)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := SmartCardReader
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRODUCT_MODULE := true
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.appcompat_appcompat \
+    androidx-constraintlayout_constraintlayout
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    libsecurespaces \
+    libsscm \
+    gson
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/src/main/res
+
+LOCAL_MANIFEST_FILE := src/main/AndroidManifest.xml
+
+LOCAL_USE_AAPT2 := true
+
+include $(BUILD_PACKAGE)
+
+endif
